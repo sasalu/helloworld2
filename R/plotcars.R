f1dfc8d64450ef177360ad65c870f87d8efbfdb1
sasalu <lukacova.alexandra@gmail.com>
2014-08-07 14:44:38 +0200

    dsf

--- R/plotcars.R
+++ R/plotcars.R
@@ -3,7 +3,9 @@
 
 plotcars <- function () {
 		
-	
-	plot(myfile)  
+	con <- dbConnect(MySQL(), host="127.0.0.1", port= 3306, user="root", password = "sasa", dbname="sasa")
+	res <- dbSendQuery(con, "SELECT * FROM teplota")
+	data = fetch(res, n=-1)
+	plot(data)
 	invisible();
 }
