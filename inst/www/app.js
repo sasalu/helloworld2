d6e69adbc85754d3ce35596f203ca1be0e80679e
sasalu <lukacova.alexandra@gmail.com>
2014-08-08 18:44:34 +0200

    fe

--- inst/www/app.js
+++ inst/www/app.js
@@ -14,20 +14,45 @@ $(function(){
     });
   });
   
-  //plot the default values on page load
-  $("#plotbutton").trigger("click")
- 
- //button handler
-  $("#uploadbutton").on("click", function(e){
-    e.preventDefault();
-    $("#uploadbutton").attr("disabled", "disabled")
-    var req = $("#plotdiv").rplot("uploadcars", {
-      }).always(function(){
-      $("#uploadbutton").removeAttr("disabled");
-    }).fail(function(){
-      alert("HTTP error " + req.status + ": " + req.responseText);
+
+ //because read.csv is in utils
+ocpu.seturl("//public.opencpu.org/ocpu/library/utils/R")
+
+//actual handler
+$("#submitbutton").on("click", function(){
+
+    //arguments
+    var myheader = $("#header").val() == "true";
+    var myfile = $("#csvfile")[0].files[0];
+        
+    if(!myfile){
+        alert("No file selected.");
+        return;
+    }
+
+    //disable the button during upload
+    $("#submitbutton").attr("disabled", "disabled");
+
+    //perform the request
+    var req = ocpu.call("read.csv", {
+        "file" : myfile,
+        "header" : myheader
+    }, function(session){
+        session.getConsole(function(outtxt){
+            $("#output").text(outtxt); 
+        });
     });
-  });
+        
+    //if R returns an error, alert the error message
+    req.fail(function(){
+        alert("Server error: " + req.responseText);
+    });
+    
+    //after request complete, re-enable the button 
+    req.always(function(){
+        $("#submitbutton").removeAttr("disabled")
+    });        
+});    
   
 });
 
