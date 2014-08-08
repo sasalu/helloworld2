c05a09e8675f9d862b03740f467beb1d4f7d1929
sasalu <lukacova.alexandra@gmail.com>
2014-08-08 18:22:23 +0200

    dsg

--- inst/www/app.js
+++ inst/www/app.js
@@ -17,30 +17,15 @@ $(function(){
   //plot the default values on page load
   $("#plotbutton").trigger("click")
   
-  var req = ocpu.rpc("tv", {input : data}, function(output){
-//repopulate the table
-$("tbody tr").each(function(i){
-$(this).find(".agefield").val(output[i].age);
-$(this).find(".maritalfield").val(output[i].marital);
-$(this).find(".tvfield").val(output[i].tv);
-});
-}).fail(function(){
-alert(req.responseText);
-});
-
-  //CSV file 
-  $("#csvfile").on("change", function loadfile(e){
-if(!$("#csvfile").val()) return;
-$("#outputcsv").addClass("hide").attr("href", "");
-
-var req = ocpu.call("tv", {
-input : $("#csvfile")[0].files[0]
-}, function(tmp){
-$("#outputcsv").removeClass("hide").attr("href", tmp.getLoc() + "R/.val/csv")
-}).fail(function(){
-alert(req.responseText)
-}).always(function(){});
-});
-  
 }
 );
+
+//call R function: stats::sd(x=data)
+$("#uploadbutton").click(function(){
+    
+    var req = ocpu.rpc("uploadcars",{
+  
+    
+    });
+	); 
+});
