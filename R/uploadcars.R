1ccef0675cd0901a0afd6a3b0c99e7651390e46a
sasalu <lukacova.alexandra@gmail.com>
2014-08-08 18:37:44 +0200

    wfa

--- R/uploadcars.R
+++ R/uploadcars.R
@@ -6,5 +6,6 @@ uploadcars <- function () {
 	con <- dbConnect(MySQL(), host="127.0.0.1", port= 3306, user="root", password = "sasa", dbname="sasa")
 	res <- dbSendQuery(con, "INSERT INTO teplota (idteplota) VALUES (2)")
 	data = fetch(res, n=-1)
+plot(data)
 	invisible();
 }
