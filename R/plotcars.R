27c77df16efa9aec0b6d2a7b7283549da559dea6
sasalu <lukacova.alexandra@gmail.com>
2014-08-08 15:03:37 +0200

    tfrh

--- R/plotcars.R
+++ R/plotcars.R
@@ -3,10 +3,7 @@
 
 plotcars <- function () {
 		
-  drv = dbDriver('MySQL')
-  con = dbConnect(drv,dbname='u590493722_sasa',user='sasa',pass='sasa12')
-
-	con <- dbConnect(MySQL(), host="127.0.0.1", port= 3306, user="root", password = "sasa", dbname="sasa")
+ 	con <- dbConnect(MySQL(), host="127.0.0.1", port= 3306, user="root", password = "sasa", dbname="sasa")
 	res <- dbSendQuery(con, "SELECT * FROM teplota")
 	data = fetch(res, n=-1)
 	plot(data)
