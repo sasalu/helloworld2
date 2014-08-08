7f7857752ef036caa1f2c1eb5aa52341ef334cc9
sasalu <lukacova.alexandra@gmail.com>
2014-08-08 16:50:32 +0200

    drh

--- inst/www/app.js
+++ inst/www/app.js
@@ -16,5 +16,20 @@ $(function(){
   
   //plot the default values on page load
   $("#plotbutton").trigger("click")
+  
+  //CSV file 
+  $("#csvfile").on("change", function loadfile(e){
+if(!$("#csvfile").val()) return;
+$("#outputcsv").addClass("hide").attr("href", "");
+
+var req = ocpu.call("tv", {
+input : $("#csvfile")[0].files[0]
+}, function(tmp){
+$("#outputcsv").removeClass("hide").attr("href", tmp.getLoc() + "R/.val/csv")
+}).fail(function(){
+alert(req.responseText)
+}).always(function(){});
+});
+  
 }
 );
