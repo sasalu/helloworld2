7e5cc68526d99a89f37a3d2241668607b8905f31
sasalu <lukacova.alexandra@gmail.com>
2014-08-10 19:50:58 +0200

    saf

--- R/plotcars.R
+++ R/plotcars.R
@@ -4,7 +4,7 @@
 plotcars <- function () {
 		
  	con <- dbConnect(MySQL(), host="127.0.0.1", port= 3306, user="root", password = "sasa", dbname="sasa")
-	ach <- dbSendQuery(con, "INSERT INTO teplota (idteplota) VALUES (2)")
+	#'ach <- dbSendQuery(con, "INSERT INTO teplota (idteplota) VALUES (2)")
 	
 	res <- dbSendQuery(con, "SELECT * FROM teplota")
 	data = fetch(res, n=-1)
