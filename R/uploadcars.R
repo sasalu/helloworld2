c15f79d07bb66c3150594e50bd88ce866707a5eb
sasalu <lukacova.alexandra@gmail.com>
2014-08-11 13:50:11 +0200

    sf

--- R/uploadcars.R
+++ R/uploadcars.R
@@ -7,6 +7,6 @@ uploadcars <- function () {
 	con <- dbConnect(MySQL(), host="127.0.0.1", port= 3306, user="root", password = "sasa", dbname="sasa")
 	
 	dbWriteTable(con, "teplota", x) 
-	ach <- dbSendQuery(con, "INSERT INTO teplota (idteplota) VALUES (7)")
+	#'ach <- dbSendQuery(con, "INSERT INTO teplota (idteplota) VALUES (7)")
 
 }
