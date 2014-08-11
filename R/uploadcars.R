3190c879dd9133a945d2f6b81b8f568e03a49613
sasalu <lukacova.alexandra@gmail.com>
2014-08-11 15:26:12 +0200

    reg

--- R/uploadcars.R
+++ R/uploadcars.R
@@ -3,11 +3,11 @@
 
 uploadcars <- function () {
 	
-	x <- read.csv(file="C:/Users/Alexandra/Documents/GitHub/helloworld2/cars.csv", header = FALSE)
+	x <- read.csv(file="C:/Users/Alexandra/Documents/GitHub/helloworld2/pokus.csv", header = FALSE)
 	con <- dbConnect(MySQL(), host="127.0.0.1", port= 3306, user="root", password = "sasa", dbname="sasa")
 	#'del <- dbSendQuery(con, "DROP DATABASE sasa")	
 	#'add <- dbSendQuery(con, "CREATE DATABASE sasa")	
-	dbWriteTable(con, "teplota", x) 
+	dbWriteTable(con, "parametre", x) 
 	#'ach <- dbSendQuery(con, "INSERT INTO teplota (idteplota) VALUES (7)")
 
 }
