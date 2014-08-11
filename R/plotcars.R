72180961ded70abe1ca67fb5d07d6b3e3dd7d921
sasalu <lukacova.alexandra@gmail.com>
2014-08-11 17:56:14 +0200

    dgf

--- R/plotcars.R
+++ R/plotcars.R
@@ -5,7 +5,7 @@ plotcars <- function () {
 		
  	con <- dbConnect(MySQL(), host="127.0.0.1", port= 3306, user="root", password = "sasa", dbname="sasa")
 	
-	res <- dbSendQuery(con, "SELECT a FROM parametre")
+	res <- dbSendQuery(con, "SELECT a FROM parametre WHERE a>80")
 	data = fetch(res, n=-1)
 	plot(data)
 
