ee633021579b900e8778d31a62ded3302dac97a0
sasalu <lukacova.alexandra@gmail.com>
2014-08-11 13:34:08 +0200

    df

--- R/uploadcars.R
+++ R/uploadcars.R
@@ -4,8 +4,9 @@
 uploadcars <- function () {
 	
 	x <- read.csv(file="C:/Users/Alexandra/Documents/GitHub/helloworld2/cars.csv")
-	
 	con <- dbConnect(MySQL(), host="127.0.0.1", port= 3306, user="root", password = "sasa", dbname="sasa")
-	ach <- dbSendQuery(con, "INSERT INTO teplota (idteplota) VALUES (5)")
+	
+	dbWriteTable(con, "teplota", x) ## table from a data.frame
+	ach <- dbSendQuery(con, "INSERT INTO teplota (idteplota) VALUES (6)")
 
 }
