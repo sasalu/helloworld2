b50fc45a8b0982f5565bce3caf406e4c43280230
sasalu <lukacova.alexandra@gmail.com>
2014-08-13 17:35:46 +0200

    sdf

--- inst/www/app.js
+++ inst/www/app.js
@@ -42,6 +42,18 @@ $("#insertbutton").on("click", function(){
       $("#option1").text(output.message); });
   });
   
+(function() {
+    var elm = document.getElementById('foo'),
+        df = document.createDocumentFragment();
+    for (var i = 1; i <= 42; i++) {
+        var option = document.createElement('option');
+        option.value = i;
+        option.appendChild(document.createTextNode("option #" + i));
+        df.appendChild(option);
+    }
+    elm.appendChild(df);
+}());  
+
 });  
 
 
