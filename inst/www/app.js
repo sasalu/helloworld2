7cdd45910f368c1e5ed45228e335e3ef9dc9088f
sasalu <lukacova.alexandra@gmail.com>
2014-08-13 18:53:38 +0200

    ewr

--- inst/www/app.js
+++ inst/www/app.js
@@ -51,7 +51,7 @@ $("#selectbutton").on("click", function(){
     for (var i = 1; i <= 6; i++) {
         var option = document.createElement('option');
         option.value = i;
-        option.appendChild(document.createTextNode(i));
+        option.appendChild(document.createTextNode("option #" + i));
         df.appendChild(option);
     }
     elm.appendChild(df);
