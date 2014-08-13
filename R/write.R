58162ae9f6b36d7767e2fd10996da3604a7783f3
sasalu <lukacova.alexandra@gmail.com>
2014-08-13 16:54:13 +0200

    gfrd

--- R/write.R
+++ R/write.R
@@ -6,6 +6,6 @@ write <- function () {
 	con <- dbConnect(MySQL(), host="127.0.0.1", port= 3306, user="root", password = "sasa", dbname="sasa")
 	sel <- dbSendQuery(con, "select column_name from information_schema.columns where table_name='parametre'")
 	data2 = fetch(sel, n=-1)
-	list(print(data2[3,1]))
+	list(message = print(data2[3,1]))
 
 }
