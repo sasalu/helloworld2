b1437f79b0591442eec1e4b2146366e68dd1e83c
sasalu <lukacova.alexandra@gmail.com>
2014-11-21 14:16:55 +0100

    ger

--- R/plotcars.R
+++ R/plotcars.R
@@ -1,11 +1,11 @@
-#' Plot cars
+#' Plot 
 #' 
 
 plotcars <- function () {
 		
  	con <- dbConnect(MySQL(), host="127.0.0.1", port= 3306, user="root", password = "sasa", dbname="sasa")
 	
-	res <- dbSendQuery(con, "SELECT a FROM parametre")
+	res <- dbSendQuery(con, "SELECT x FROM parametre")
 	data = fetch(res, n=-1)
 	plot(data)
 
