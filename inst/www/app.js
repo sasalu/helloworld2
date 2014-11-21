611ac24872b4a4db786dc21ce17631867fec2df0
sasalu <lukacova.alexandra@gmail.com>
2014-11-21 13:52:14 +0100

    rwr

--- inst/www/app.js
+++ inst/www/app.js
@@ -14,11 +14,25 @@ $(function(){
     });
   });
   
+$("#insertbutton").on("click", function(){
+   
+   var x = ocpu.call("uploadcars", {
+		file : myfile,
+		header : myheader,
+		sep : selectedSep
+      }).always(function(){
+      $("#insertbutton").removeAttr("disabled");
+    }).fail(function(){
+      alert("HTTP error " + x.status + ": " + x.responseText);
+    });
+  });
+
+}
+ 
+
 $("#selectbutton").on("click", function(){
     
-	var z = ocpu.rpc("uploadcars", {}, function(output){});
-   
-    var elm = document.getElementById('attribute'),
+	var elm = document.getElementById('attribute'),
         df = document.createDocumentFragment();
     for (var i = 1; i <= 6; i++) {
         var option = document.createElement('option');
@@ -29,7 +43,6 @@ $("#selectbutton").on("click", function(){
     elm.appendChild(df);
 }());
   
-   
   
 });  
 
