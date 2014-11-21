4d03e72812c35b1c4e0c9fcc1ac8ae1bedb49a60
sasalu <lukacova.alexandra@gmail.com>
2014-11-21 15:13:42 +0100

    esg

--- R/plotcars.R
+++ R/plotcars.R
@@ -5,9 +5,8 @@ plotcars <- function () {
 		
  	dbhandle <- odbcDriverConnect('driver={SQL Server};server=SASA;database=sasa;trusted_connection=true')
 	
-	res <- dbSendQuery(dbhandle, "SELECT x FROM parametre")
-	data = fetch(res, n=-1)
-	plot(data)
+	res <- sqlQuery(dbhandle, "SELECT x FROM parametre")
+	plot(res)
 
 	invisible();
 }
