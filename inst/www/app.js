51b739d0a9e56b3f1df704c964fb3a9a8f640c80
sasalu <lukacova.alexandra@gmail.com>
2014-11-21 13:14:57 +0100

    fds

--- inst/www/app.js
+++ inst/www/app.js
@@ -14,38 +14,6 @@ $(function(){
     });
   });
   
-$("#insertbutton").on("click", function(){
-    
-	var myheader = $("#header").val() == "true";
-	var mysep = document.getElementsByName("sep");
-
-	for(var i = 0; i < mysep.length; i++) {
-	if(mysep[i].checked == true) {
-       selectedSep = mysep[i].value;
-   }
- }
-	var myfile = $("#csvfile")[0].files[0];
-    if(!myfile){ alert("No file selected."); return; }
-	
-	$("#insertbutton").attr("disabled", "disabled");
-	
-    var x = ocpu.call("uploadcars", {
-		file : myfile,
-		header : myheader,
-		sep : selectedSep
-      }).always(function(){
-      $("#insertbutton").removeAttr("disabled");
-    }).fail(function(){
-      alert("HTTP error " + x.status + ": " + x.responseText);
-    });
-  });
-
-function p(){
-    var z = ocpu.rpc("write", {}, function(output){output.message});
-    return z;
-}
- 
-
 $("#selectbutton").on("click", function(){
     
 	
